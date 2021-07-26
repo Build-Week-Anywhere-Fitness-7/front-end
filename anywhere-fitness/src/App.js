134e6cefb4ac130f464ebf2019fab9d188c52a73
kennymiesner <kennymiesner@gmail.com>
2021-07-26 17:20:58 -0500

    Create login component

--- anywhere-fitness/src/App.js
+++ anywhere-fitness/src/App.js
@@ -3,13 +3,17 @@ import { Switch, Route } from 'react-router-dom'
 import axios from 'axios'
 import * as yup from 'yup'
 import FitHome from './components/Home'
+import Login from './components/Login'
 import './App.css';
 
 export default function App() {
   return (
     <>
       <Switch>
-        <Route path=''>
+        <Route path='/login'>
+          <Login />
+        </Route>
+        <Route path='/'>
           <FitHome />
         </Route>
       </Switch>
--- anywhere-fitness/src/components/Login.js
+++ anywhere-fitness/src/components/Login.js
@@ -1,13 +1,13 @@
 import { Link, useRouteMatch } from 'react-router-dom'
 import React from 'react'
 
-export default function FitHome() {
+export default function Login() {
   const { url } = useRouteMatch()
 
   return (
     <div className='mainContainer'>
-      <h1>Fitness</h1>
-      <Link to={`${url}login`}><button id='redirect'>Login</button></Link>
+      <h1>Login</h1>
+      <Link to={`/`}><button id='redirect'>Back Home</button></Link>
     </div>
   )
 }
