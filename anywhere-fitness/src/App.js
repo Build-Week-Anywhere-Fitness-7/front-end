18d54d502441c9ed2143267d22569fc5f397ad16
kennymiesner <kennymiesner@gmail.com>
2021-07-27 17:35:59 -0500

    json-server fake api wip

--- anywhere-fitness/src/App.js
+++ anywhere-fitness/src/App.js
@@ -5,32 +5,41 @@ import * as yup from 'yup'
 import FitHome from './components/Home'
 import Login from './components/Login'
 import SignUp from './components/SignUp'
-import Nav from './components/Nav'
 import Classes from './components/Classes'
 import CreateAccount from './components/CreateAccount'
 import './App.css';
 
 export default function App() {
   return (
-    <>
-      <Nav />
-      <Switch>
-        <Route path='/classes'>
-          <Classes /> 
-        </Route>
-        <Route path='/create-account'>
-          <CreateAccount /> 
-        </Route>
-        <Route path='/sign-up'>
-          <SignUp />
-        </Route>
-        <Route path='/login'>
-          <Login />
-        </Route>
-        <Route path='/'>
-          <FitHome />
-        </Route>
-      </Switch>
+    <>    
+    <div className='header'>
+      <nav>
+        <Link to='/'>Home</Link>
+        <Link to='/Login'>Login</Link>
+        <Link to='/SignUp'>Sign Up</Link>
+        <Link to='/Profile'>Profile</Link>
+        <Link to={''}>Logout</Link>
+      </nav>
+      <title>Anytime Fitness</title>
+    </div>
+
+    <Switch>
+      <Route path='/classes'>
+        <Classes />
+      </Route>
+      <Route path='/CreateAccount'>
+        <CreateAccount />
+      </Route>
+      <Route path='/SignUp'>
+        <SignUp />
+      </Route>
+      <Route path='/Login'>
+        <Login />
+      </Route>
+      <Route path='/'>
+        <FitHome />
+      </Route>
+    </Switch>
     </>
   )
 }
--- anywhere-fitness/src/components/Classes.js
+++ anywhere-fitness/src/components/Classes.js
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react'
+import { useParams } from 'react-router'
+import axios from 'axios'
 import {
   Container,
   Card,
@@ -10,10 +12,30 @@ import {
 } from 'reactstrap';
 
 const Classes = (props) => {
+  const [details, setDetails]  = useState();
+  const { id } = useParams();
+
+  useEffect(() => {
+    axios
+      .get(`http://localhost:3001/classes/${id}`)
+      .then(res => {
+        setDetails(res.data)
+        console.log(res.data)
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }, [id])
+
+  // if (!workout) {
+  //   return <h3>Working on fetching available class details...</h3>
+  // }
+
   return (
-    <Container>
+    <Container className='Classes'>
+      <h1>Classes</h1>
       <Row>
-        <Col sm="3">
+        <Col sm="4">
           <Card body>
             <CardTitle tag="h3">Outdoor Yoga</CardTitle>
             <CardText><strong>Type:</strong> Yoga</CardText>
@@ -25,55 +47,20 @@ const Classes = (props) => {
             <Button color='primary'>Book</Button>
           </Card>
         </Col>
-        <Col sm="3">
+        {/* <Col sm="4">
           <Card body>
-            <CardTitle tag="h3">Outdoor Yoga</CardTitle>
-            <CardText><strong>Type:</strong> Yoga</CardText>
-            <CardText><strong>Start Time:</strong> 5:30pm</CardText>
-            <CardText><strong>Duration:</strong> 45min</CardText>
-            <CardText><strong>Intensity Level:</strong> Moderate</CardText>
-            <CardText><strong>Location:</strong> Terrace Park</CardText>
-            <CardText><strong>Booked:</strong> 5/25</CardText>
+            <CardTitle tag="h3">{details.name}</CardTitle>
+            <CardText><strong>Type:</strong> {details.type}</CardText>
+            <CardText><strong>Start Time:</strong> {details.time}</CardText>
+            <CardText><strong>Duration:</strong> {details.duration}</CardText>
+            <CardText><strong>Intensity Level:</strong> {details.intensity}</CardText>
+            <CardText><strong>Location:</strong> {details.location}</CardText>
+            <CardText><strong>Booked:</strong>  {details.enrolled}/{details.max}</CardText>
             <Button color='primary'>Book</Button>
           </Card>
-        </Col>
-        <Col sm="3">
-          <Card body>
-            <CardTitle tag="h3">Outdoor Yoga</CardTitle>
-            <CardText><strong>Type:</strong> Yoga</CardText>
-            <CardText><strong>Start Time:</strong> 5:30pm</CardText>
-            <CardText><strong>Duration:</strong> 45min</CardText>
-            <CardText><strong>Intensity Level:</strong> Moderate</CardText>
-            <CardText><strong>Location:</strong> Terrace Park</CardText>
-            <CardText><strong>Booked:</strong> 5/25</CardText>
-            <Button color='primary'>Book</Button>
-          </Card>
-        </Col>
-        <Col sm="3">
-          <Card body>
-            <CardTitle tag="h3">Outdoor Yoga</CardTitle>
-            <CardText><strong>Type:</strong> Yoga</CardText>
-            <CardText><strong>Start Time:</strong> 5:30pm</CardText>
-            <CardText><strong>Duration:</strong> 45min</CardText>
-            <CardText><strong>Intensity Level:</strong> Moderate</CardText>
-            <CardText><strong>Location:</strong> Terrace Park</CardText>
-            <CardText><strong>Booked:</strong> 5/25</CardText>
-            <Button color='primary'>Book</Button>
-          </Card>
-        </Col>
+        </Col> */}
       </Row>
     </Container>
-
-    // REQUIRED DATA POINTS
-    //
-    // - `Name`
-    // - `Type`
-    // - `Start time`
-    // - `Duration`
-    // - `Intensity level`
-    // - `Location`
-    // - `Current number of registered attendees`
-    // - `Max class size`
   );
 };
 
